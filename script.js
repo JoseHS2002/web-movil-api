e92e91276446d13cfe10648d59c9ba4e872830e2
JoseHS2002 <joherso16@gmail.com>
2024-12-09 23:47:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+<script>
+        document.getElementById('fetch-button').addEventListener('click', function() {
+            fetch('https://rickandmortyapi.com/api/character')
+                .then(response => response.json())
+                .then(data => {
+                    const characterList = document.getElementById('character-list');
+                    characterList.innerHTML = '';
+                    data.results.forEach(character => {
+                        const div = document.createElement('div');
+                        div.classList.add('character');
+                        div.innerHTML = `<strong>Nombre: ${character.name}</strong><br>Estado: ${character.status}<br>Especie: ${character.species}<br>Genero: ${character.gender}`;
+                        characterList.appendChild(div);
+                    });
+                })
+                .catch(error => console.error('Error fetching data:', error));
+        });
+</script>
